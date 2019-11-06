e51586bf4f5c17ea9f1672d202a14bc75b058efe
Christoph Guttandin <chrisguttandin@media-codings.com>
2019-11-06 01:18:16 +0100

    fix: remove useless import

--- src/types/instantiate-encoder-function.ts
+++ src/types/instantiate-encoder-function.ts
@@ -1,3 +1,1 @@
-import { TInstantiateEncoderFunction } from './instantiate-encoder-function';
-
 export type TInstantiateEncoderFunction = (encoderId: number, mimeType: string, sampleRate: number) => MessagePort;
